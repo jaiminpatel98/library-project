f54f52356f4c18f4cb87826e1e84e1a9f09277ee
jaiminpatel98 <jaiminpatel98@gmail.com>
2023-10-17 09:20:01 -0700

    video-service api endpoint

--- video-service/src/index.ts
+++ video-service/src/index.ts
@@ -1,12 +1,59 @@
-import express from "express";
+import express from 'express';
+import { convertVideo, deleteProcessedVideo, deleteRawVideo, downloadRawVideo, setupDirectories, uploadProcessedVideo } from './gcStorage';
+import { inflate } from 'zlib';
+
+const ffmpegStatic = require('ffmpeg-static');
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegStatic);
+
+setupDirectories();
 
 const app = express();
-const port = 3000
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send("Hello World")
+app.post('/process-video', async (req, res) => {
+  // Get the bukcet and filename from the Cloud Pub/Sub message
+  let data;
+  try {
+    const message = Buffer.from(req.body.message.data, 'base64').toString('utf8');
+    data = JSON.parse(message);
+    if (!data.name) {
+      throw new Error('Invalid message payload received.');
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send('Bad request: missing fileName.');
+  }
+
+  const inFileName = data.name;
+  const outFileName = `processed-${inFileName}`;
+
+  // Download raw video from CS
+  await downloadRawVideo(inFileName);
+
+  try {
+    await convertVideo(inFileName, outFileName);
+  } catch(error) {
+    await Promise.all([
+      deleteRawVideo(inFileName),
+      deleteProcessedVideo(outFileName)
+    ])
+    console.log(error)
+    return res.status(500).send('Internal Server error: Video conversion processing failed.')
+  }
+
+  // Upload the processed video to CS
+  await uploadProcessedVideo(outFileName);
+
+  await Promise.all([
+    deleteRawVideo(inFileName),
+    deleteProcessedVideo(outFileName)
+  ])
+
+  return res.status(200).send('Video conversion processsing finished successfully.')
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('Video service listening at http://localhost:${port}')
-})
+  console.log(`Video service listening at http://localhost:${port}`)
+});
