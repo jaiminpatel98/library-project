fecdce8b838695c46bdef5a1f9c97b1aa807f401
jaiminpatel98 <jaiminpatel98@gmail.com>
2023-10-19 16:05:15 -0700

    Added 2 new GC Functions generateUploadUrl,getVideos

--- library-api-service/functions/src/index.ts
+++ library-api-service/functions/src/index.ts
@@ -2,9 +2,25 @@ import * as functions from "firebase-functions";
 import {initializeApp} from "firebase-admin/app";
 import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
 
 initializeApp();
+
 const firestore = new Firestore();
+const storage = new Storage();
+const rawVideoBucketName = "library-raw-videos";
+const videoCollectionId = "videos";
+
+export interface Video {
+  id?: string,
+  uid?: string,
+  fileName?: string,
+  status?: "processing" | "processed",
+  title?: string,
+  description?: string,
+  date?: string
+}
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -17,4 +33,32 @@ export const createUser = functions.auth.user().onCreate((user) => {
   logger.info(`User Created: ${JSON.stringify(userInfo)}`);
 
   return;
+});
+
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
+  if (!request.auth) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "Function must be called while user is authenticated."
+    );
+  }
+
+  const auth = request.auth;
+  const data = request.data;
+  const bucket = storage.bucket(rawVideoBucketName);
+
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 15 * 60 * 1000,
+  });
+
+  return {url, fileName};
+});
+
+export const getVideos = onCall({maxInstances: 1}, async () => {
+  const querySnapshot =
+    await firestore.collection(videoCollectionId).limit(10).get();
+  return querySnapshot.docs.map((doc) => doc.data());
 });
