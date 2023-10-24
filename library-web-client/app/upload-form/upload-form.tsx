b1874846af7b73c415dd212c508fa55effa62a9f
jaiminpatel98 <jaiminpatel98@gmail.com>
2023-10-24 12:25:51 -0700

    Replaced upload file functionality with standalone cmp

--- library-web-client/app/upload-form/upload-form.tsx
+++ library-web-client/app/upload-form/upload-form.tsx
@@ -1,29 +1,20 @@
 'use client';
-import React, { Fragment } from "react";
+import React,  { Fragment, useState } from "react";
 import * as Dialog from '@radix-ui/react-dialog';
 import { uploadVideo } from "../utilities/firebase/functions";
 import { Cross2Icon } from '@radix-ui/react-icons';
+import UploadInput from '../components/upload-input';
 import styles from "./upload-form.module.css";
 
 export default function UploadForm() {
-  const [file, setFile] = React.useState<File>();
-  const [title, setTitle] = React.useState<string>();
-  const [description, setDescription] = React.useState<string>();
-  const [fileName, setFileName] = React.useState<string>();
+  const [file, setFile] = useState<File>();
+  const [title, setTitle] = useState<string>();
+  const [description, setDescription] = useState<string>();
 
   const handleDescriptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newDescription = event.target.value;
     if (newDescription) {
       setDescription(newDescription);
-    }
-  }
-
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newFile = event.target.files?.item(0);
-    if (newFile) {
-      setFile(newFile);
-      console.log(JSON.stringify(newFile.name))
-      setFileName(newFile.name);
     }
   }
 
@@ -41,7 +32,7 @@ export default function UploadForm() {
     }
     try {
       //TODO: conditionally call upload function based on media type - video, audio, image
-      //TODO: Establish interfaces as global types - Video, Upload - then we can just send upload to the function
+      //TODO: Establish interfaces as global types - Video, Upload - then we can just send upload interface to the function
       const response = await uploadVideo(file, title, description);
       alert(`File uploaded successfully. Response: ${JSON.stringify(response)}`);
     } catch (error) {
@@ -53,10 +44,8 @@ export default function UploadForm() {
     <Fragment>
       <Dialog.Root>
         <Dialog.Trigger asChild>
-          <button className={styles.triggerButton}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-            </svg>
+          <button className="Button slate">
+            Upload
           </button>
         </Dialog.Trigger>
         <Dialog.Portal>
@@ -81,14 +70,7 @@ export default function UploadForm() {
                 onChange={handleDescriptionChange}/>
             </fieldset>
             <fieldset className="Fieldset">
-              <label htmlFor="upload" className={styles.triggerButton}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
-                </svg>
-              </label>
-              <input id="upload" className={styles.uploadInput} type="file" accept="video/*"
-                onChange={handleFileChange} />
-              <p>{fileName}</p>
+              <UploadInput stateChanger={setFile} />
             </fieldset>
             <div className={styles.confirmButtonDiv}>
               <Dialog.Close asChild>
