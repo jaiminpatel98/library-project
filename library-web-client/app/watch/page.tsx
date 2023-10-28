506aecf670dea935a8fa2b0eecf8c63bb475f157
jaiminpatel98 <jaiminpatel98@gmail.com>
2023-10-28 13:07:51 -0700

    Update watch page to display video or audio

--- library-web-client/app/watch/page.tsx
+++ library-web-client/app/watch/page.tsx
@@ -2,13 +2,22 @@
 import { useSearchParams } from "next/navigation";
 
 export default function Watch() {
-  const videoPrefix = 'https://storage.googleapis.com/library-processed-videos/'
+  const videoPrefix = 'https://storage.googleapis.com/library-processed-videos/';
+  const audioPrefix = 'https://storage.googleapis.com/library-audios/';
   const videoSrc = useSearchParams().get('v');
-
+  const audioSrc = useSearchParams().get('a');
+  
   return (
     <div>
       <h1>Watch Page</h1>
-      { <video controls src={videoPrefix + videoSrc}/> }
+      {
+        videoSrc &&
+        <video controls src={videoPrefix + videoSrc}/> 
+      }
+      {
+        audioSrc &&
+        <audio controls src={audioPrefix + audioSrc}/>
+      }
     </div>
   );
 }
