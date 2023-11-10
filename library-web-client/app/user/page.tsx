6973b1e28ecf1968a9e84f6a23ee8bec9bc3909f
jaiminpatel98 <jaiminpatel98@gmail.com>
2023-11-10 13:44:53 -0800

    New user page

---
+++ library-web-client/app/user/page.tsx
@@ -0,0 +1,60 @@
+'use client';
+import styles from './page.module.css';
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { getUser } from "../utilities/firebase/functions";
+import { Bars } from  'react-loader-spinner';
+
+export default function User() {
+  const [user, setUser] = useState<User>();
+  const [loading, setLoading] = useState(false);
+  const uid = useSearchParams().get('u');
+
+  useEffect(() => {
+    if (uid && !user) {
+      setLoading(true);
+      getUser(uid)
+      .then((res) => {
+        if (res) {
+          setUser(res);
+          setLoading(false);
+        }
+        console.log(res)
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log(error)
+      })
+    }
+  })
+
+  return (
+    <div className={styles.page}>
+      {
+        !loading &&
+        <div>
+          <h1>{user ? user.name : ''}</h1>
+          {
+            user?.photoUrl &&
+            <img src={user.photoUrl} height={360}/>
+          }
+          <p>{user ? user.bio : ''}</p>
+        </div>
+      }
+      {
+        loading &&
+        <div className={styles.loading}>
+          <Bars
+          height="80"
+          width="80"
+          color="#222222"
+          ariaLabel="bars-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          />
+        </div>
+      }
+    </div>
+  );
+}
